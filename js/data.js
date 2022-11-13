122e9ea035fce9ba77e89fa2a85fed5134d7e065
Carles <carlevil@espol.edu.ec>
2022-11-12 23:42:47 -0500

    Probando programa

--- js/data.js
+++ js/data.js
@@ -1,31 +1,22 @@
 var data = {
   name : [
-    {name: 'Name', value: 'Ahsan Khan'}
+    {name: 'Name', value: 'Carlos Villafuerte'}
   ],
   bio: [
-    {name: 'Name', value: 'Ahsan Khan'},
-    {name: 'Email', value: 'm.ahsankhan26@hotmail.com'},
-    {name: 'Phone', value: '+92-344-0200963'},
-    {name: 'Country', value: 'Pakistan'},
-    {name: 'City', value: 'Karachi'}
+    {name: 'Name', value: 'Carlos Villafuerte'},
+    {name: ' Email', value: 'len.great@gmail.com'},
+    {name: ' Phone', value: '+593-999597992'},
+    {name: ' Country', value: 'Ecuador'},
+    {name: ' City', value: 'Guayaquil'}
   ],
   github: [
-    {name: 'GitHub', value: 'ahsankhan26'}
+    {name: 'GitHub', value: 'lengreat'}
   ],
-  projects: [
-    {name: 'Book Blog', value: 'https://github.com/ahsankhan26/Book-Blog'},
-    {name: 'Bomberman Replica', value: 'https://github.com/ahsankhan26/Bomberman-Replica'},
-    {name: 'WhatsApp Chat Parser', value: 'https://github.com/ahsankhan26/Whatsapp-Chat-Parser'},
-    {name: 'Laser Data Transmission', value: 'https://github.com/ahsankhan26/Laser-Data-Transmission'},
-    {name: 'Color Guessing Game', value: 'https://github.com/ahsankhan26/Color-Guessing-Game'},
-    {name: 'GPA Calculator', value: 'https://github.com/ahsankhan26/gpa-calculator'},
-    {name: 'Hello World but unnecessarily complex', value: 'https://github.com/ahsankhan26/hello-world'}
-  ],
-  resume: 'Ahsan_Khan_CV.pdf',
+  projects: [],
+  resume: 'CV-CarlosVillafuerteP.pdf',
   socials: [
-    {name: 'LinkedIn', value: 'https://linkedin/in/ahsankhan26'},
-    {name: 'GitHub', value: 'https://github.com/ahsankhan26'},
-    {name: 'Instagram', value: 'https://instagram/ahsankhan26'},
-    {name: 'Website', value: 'https://ahsankhan.me'}
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/leonardo-villafuerte1988/'},
+    {name: 'GitHub', value: 'https://github.com/lengreat'},
+    {name: 'Instagram', value: 'https://www.instagram.com/len.great/'},
   ]
 }
